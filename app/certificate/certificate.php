9647e57908802161242d032d7633157bf5c1b758
janhamedi49@gmail.com <janhamedi49@gmail.com>
2021-04-15 23:12:15 -0500

    Certificate change frame

--- app/certificate/certificate.php
+++ app/certificate/certificate.php
@@ -8,26 +8,28 @@ use App\database\Database;
 $db = Database::getInstance();
 $db_connection = $db->getConnection();
 $pdf = new FPDF('P', 'mm', array(210, 297));
-$certificateid = (isset($_GET['certificateid']) ? ($_GET['certificateid']) : "Nothing found");
+$certificateid = (isset($_GET['certificateid']) ? ($_GET['certificateid']) : null);
 // $posts = $db->select("SELECT * FROM student WHERE nim = 12926861659608 LIMIT 1");
 $posts = $db->select("SELECT student.nim, student.name 
-                        AS sname, student.lastname AS slastname,
-                         certificate.certificate_name,
-                         courses.id AS courseid,
-                         courses.title AS coursetitle,
-                         event.id,
-                         event.title AS eventTitle,
-                         event.speaker 
-                         FROM student INNER JOIN certificate ON 
-                         student.nim=certificate.student_id
-                         INNER  JOIN courses ON certificate.courses_id=courses.id
-                         INNER  JOIN event ON certificate.event_id=event.id
-                         WHERE certificate.id = " . $certificateid . " LIMIT 1");
-$pdf->addPage("P", "A4");
-$pdf->GetPageWidth();  // Width of Current Page
-$pdf->GetPageHeight(); // Height of Current Page
-$pdf->SetAutoPageBreak(true, 1);
-$pdf->SetFont('Arial', 'B', 16);
+    AS sname, student.lastname AS slastname,
+     certificate.certificate_name,
+     courses.id AS courseid,
+     courses.title AS coursetitle,
+     event.id,
+     event.title AS eventTitle,
+     event.speaker 
+     FROM student INNER JOIN certificate ON 
+     student.nim=certificate.student_id
+     INNER  JOIN courses ON certificate.courses_id=courses.id
+     INNER  JOIN event ON certificate.event_id=event.id
+     WHERE certificate.id = " . $certificateid . " LIMIT 1");
+    $pdf->addPage("P", "A4");
+    $pdf->GetPageWidth();  // Width of Current Page
+    $pdf->GetPageHeight(); // Height of Current Page
+    $pdf->SetAutoPageBreak(true, 1);
+    $pdf->SetFont('Arial', 'B', 16);
+   $pdf->SetTextColor(0,0,0);
+
 if (mysqli_num_rows($posts) <= 0) {
     echo '<div class="alert alert-primary" role="alert"><h1>NO student found</h1></div>';
 } else {
@@ -35,7 +37,7 @@ if (mysqli_num_rows($posts) <= 0) {
     while ($row = mysqli_fetch_assoc($posts)) {
 
         $pdf->SetFont('', 'U');
-        $pdf->Cell(0, -90,  strtoupper($_SESSION['sname']), 0, true, 'C');
+        $pdf->Cell(0, -90,  strtoupper($row['sname']), 0, true, 'C');
         $pdf->SetFont('Courier', 'B', 12);
         
         $pdf->Text(30, 120, strtoupper($row['coursetitle']));
@@ -55,6 +57,7 @@ if (mysqli_num_rows($posts) <= 0) {
             $pdf->Text(140, 260, strtoupper($row['speaker']));
         }
         $pdf->Ln();
+
         $pdf->Output();
     }
     $posts->free();
